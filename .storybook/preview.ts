111fc2d0793d47571d9b88fff5a835d9ce9bdb03
ETBlue <etblue.tw@gmail.com>
2026-02-04 04:29:11 +0800

    feat: configure dark mode toggle for Storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,25 @@
-import type { Preview } from '@storybook/react-vite'
+import type { Preview } from '@storybook/react'
+import { withThemeByClassName } from '@storybook/addon-themes'
+import '../src/index.css'
 
 const preview: Preview = {
   parameters: {
     controls: {
       matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
+        color: /(background|color)$/i,
+        date: /Date$/i,
       },
     },
   },
-};
+  decorators: [
+    withThemeByClassName({
+      themes: {
+        light: '',
+        dark: 'dark',
+      },
+      defaultTheme: 'light',
+    }),
+  ],
+}
 
-export default preview;
+export default preview
