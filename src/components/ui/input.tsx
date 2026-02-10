a56902740e58696bba1a1dfa7efaad0efa58f763
ETBlue <etblue.tw@gmail.com>
2026-02-10 23:12:43 +0800

    refactor(input): update to new design tokens

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -8,15 +8,11 @@ const Input = React.forwardRef<HTMLInputElement, React.ComponentProps<'input'>>(
       <input
         type={type}
         className={cn(
-          `flex h-10 w-full rounded-sm px-3 py-2
-          text-foreground-default
-          bg-background-surface
-          border border-accessory-default
-          placeholder:text-foreground-muted
-          focus:outline-none focus:border-accessory-emphasized
-          disabled:cursor-not-allowed disabled:opacity-50
-          file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground-default
-          md:text-sm`,
+          `flex h-10 w-full px-2 py-0 
+          file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground 
+          placeholder:text-accessory-emphasized 
+          disabled:cursor-not-allowed disabled:opacity-50 md:text-sm
+          border border-accessory bg-background-surface `,
           className,
         )}
         ref={ref}
